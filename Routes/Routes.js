884f0bc769ca92da3cead70c911b322397310bcf
Rolando Guerrero sanpayo <rolandoguerrerosanpayo@admin.local>
2022-09-01 11:30:45 -0500

    new oposss

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express')
-const { userProduct, userProvedor, inserProvedor, inserProduct, userRegister, userLogin, getProvedor, getProductos } = require("../controller/ControllerUser")
+const { userRegister, userLogin} = require("../controller/ControllerUser")
 
 const router = Router()
 
@@ -7,16 +7,6 @@ router.post("/register",userRegister)
 
 router.post("/login",userLogin)
 
-router.get("/listmotel/:id",userProduct)
 
-router.get("/listprovesor/:id",userProvedor)
-
-router.post("/insertprovedores",inserProvedor)
-
-router.post("/insertproduct",inserProduct)
-
-router.get("/getprovedor",getProvedor)
-
-router.get("/getproductos",getProductos)
 
 module.exports={router}
